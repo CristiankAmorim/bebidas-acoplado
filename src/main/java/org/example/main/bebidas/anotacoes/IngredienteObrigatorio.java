8e6a4d77295bad1b841e2c369b61f4c228788225
Cristian Amorim <camorim1100@gmail.com>
2026-05-29 16:52:38 -0300

    Criando a anotação para marcar ingredientes como obrigatórios

---
+++ src/main/java/org/example/main/bebidas/anotacoes/IngredienteObrigatorio.java
@@ -0,0 +1,20 @@
+package org.example.main.bebidas.anotacoes;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Anotação para marcar ingredientes obrigatório para fazer uma bebida.
+ *
+ * @Target(TYPE) -> Indica que a anotação só pode ser usada em classes.
+ * @Retention(RUNTIME)  → mantida no bytecode e carregada em execução,
+ *  *                     permitindo que a Reflexão a leia via getAnnotation()
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface IngredienteObrigatorio {
+    String descricao(); // nome do ingrediente
+    String mensagemdeErro() default "Igrediente obrigatório esgotado.";
+}
